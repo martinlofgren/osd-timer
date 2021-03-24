021d918ca34269c3ada553c73e08afba17cd0e42
Martin Löfgren <martin@martinlofgren.se>
2021-03-24 23:12:43 +0100

    Initial commit, websockets and osd running but unpolished

---
+++ src/main.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+
+import asyncio
+import websockets
+import json
+import logging
+
+logging.basicConfig(level=20)
+
+users = set()
+state = {
+    'running': False,
+    'time': 60,
+}
+
+q = asyncio.Queue()
+
+async def set_state(k, v):
+    state[k] = v
+    await send_state()
+
+
+async def events_handler():
+    logging.info('events_handler started')
+    while True:
+        event = await q.get()
+        print(f'got event: {event}')
+
+
+async def update():
+    logging.info('update started')
+    while True:
+        if state['running']:
+            await set_state('time', state['time'] - 1)
+        await asyncio.sleep(1)
+
+
+async def osd(text, size, color, outline, delay):
+    cmd = f"""osd_cat \
+        --pos=top \
+        --align=right \
+        --font='-*-latin modern sans-*-r-*-*-*-{size}-*-*-*-*-*-*' \
+        --color={color} \
+        --delay={delay} \
+        --outline={outline} \
+        --indent={int(size/70)}
+    """
+    proc = await asyncio.create_subprocess_shell(
+        cmd,
+        stdin=asyncio.subprocess.PIPE)
+    await proc.communicate(text.encode('utf-8'))
+
+
+async def draw():
+    logging.info('draw started')
+    while True:
+        if state['running']:
+            secs = state['time']
+            time_string = f'{secs // 60 :02}:{secs % 60 :02}'
+            await osd(time_string, 150, 'white', 2, 1)
+        else:
+            await asyncio.sleep(1)
+
+
+async def send_state(user=None):
+    receivers = [user] if user is not None else users
+    if receivers:
+        message = json.dumps(state)
+        await asyncio.wait([receiver.send(message) for receiver in receivers])
+
+
+async def handle_message(websocket, message):
+    logging.info(f'{websocket.remote_address} sent: {message}')
+    try:
+        # await q.put('start')
+        data = json.loads(message)
+        if 'action' in data:
+            cmd = data['action']
+            if cmd == 'start':
+                await set_state('running', True)
+            elif cmd == 'stop':
+                await set_state('running', False)
+            elif cmd == 'reset':
+                await set_state('time', 0)
+
+    except:
+        await websocket.send(json.dumps({'error': f'malformed data: {message}'}))
+
+
+async def handler(websocket, path):
+    users.add(websocket)
+    logging.info(f'{websocket.remote_address} connected')
+    await send_state(websocket)
+    try:
+        async for message in websocket:
+            await handle_message(websocket, message)
+    finally:
+        users.remove(websocket)
+        logging.info(f'{websocket.remote_address} disconnected')
+
+
+start_server = websockets.serve(handler, "localhost", 8765)
+logging.info('server starting')
+
+
+try:
+    asyncio.get_event_loop().run_until_complete(start_server)
+
+    c1 = asyncio.get_event_loop().create_task(update())
+    c2 = asyncio.get_event_loop().create_task(events_handler())
+    c3 = asyncio.get_event_loop().create_task(draw())
+    asyncio.gather(c1, c2, c3)
+
+    asyncio.get_event_loop().run_forever()
+except KeyboardInterrupt:
+    print('\r', end='')
+    logging.info('server stopping')
---
+++ www/js/script.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const url = 'ws://localhost:8765'
+
+const socket = new WebSocket(url);
+
+const dom_min = document.getElementById('min');
+const dom_sec = document.getElementById('sec');
+const dom_toggle = document.getElementById('toggle');
+
+let state = {
+    running: false,
+    time: {
+        min: null,
+        sec: null,
+    }
+}
+
+function update() {
+    dom_min.value = state.time.min;
+    dom_sec.value = state.time.sec;
+    dom_min.disabled = state.running;
+    dom_sec.disabled = state.running;
+    dom_toggle.value = (state.running ? 'Stanna' : 'Starta') + ' klockan';
+}
+
+socket.addEventListener('message', event => {
+    const payload = JSON.parse(event.data);
+    state.running = payload.running;
+    state.time.min = Math.floor(payload.time / 60);
+    state.time.sec = payload.time % 60;
+    update();
+});
+
+dom_toggle.addEventListener('click', event => {
+    const action = {'action': state['running'] ? 'stop' : 'start'};
+    socket.send(JSON.stringify(action));
+});
