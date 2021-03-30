cffac36c93c96e81262940bed63cd1d6db65a8fc
Martin Löfgren <martin@martinlofgren.se>
2021-03-30 23:27:03 +0200

    Add systemd file, polish stuff

--- www/js/script.js
+++ www/js/script.js
@@ -1,13 +1,15 @@
 'use strict'
 
-const url = new URL(document.URL)
-const address = `ws://${url.hostname}:8765`
-
-const socket = new WebSocket(address);
-
 const dom_min = document.getElementById('min');
 const dom_sec = document.getElementById('sec');
 const dom_toggle = document.getElementById('toggle');
+
+const dom_time_div = document.getElementById('time');
+const dom_offline_div = document.getElementById('offline');
+
+const url = new URL(document.URL)
+const address = `ws://192.168.1.33:8765`
+// var socket;
 
 let state = {
     running: false,
@@ -25,18 +27,42 @@ function update() {
     dom_toggle.value = (state.running ? 'Stanna' : 'Starta') + ' klockan';
 }
 
-socket.addEventListener('message', event => {
-    const payload = JSON.parse(event.data);
-    state.running = payload.running;
-    state.time.min = Math.floor(payload.time / 60);
-    state.time.sec = payload.time % 60;
-    update();
-});
+function connect() {
+    const socket = new WebSocket(address);
 
-dom_toggle.addEventListener('click', event => {
-    const action = {
-        'action': state.running ? 'stop' : 'start',
-        'time': parseInt(dom_min.value) * 60 + parseInt(dom_sec.value)
-    };
-    socket.send(JSON.stringify(action));
-});
+    socket.addEventListener('message', event => {
+        const payload = JSON.parse(event.data);
+        state.running = payload.running;
+        min = payload.time / 60;
+        state.time.min = (min > 0) ? Math.floor(min) : Math.ceil(min);
+        state.time.sec = payload.time % 60;
+        update();
+    });
+
+    socket.addEventListener('open' , event => {
+        dom_offline_div.style.opacity = 0;
+        dom_time_div.style.opacity = 100;
+    });
+
+    const shit = e => {
+        dom_offline_div.style.opacity = 100;
+        dom_time_div.style.opacity = 0;
+    }
+
+    socket.addEventListener('error' , e => {
+        shit();
+        window.setTimeout(connect, 2000);
+    });
+    socket.addEventListener('close' , shit);
+
+    dom_toggle.addEventListener('click', event => {
+        const action = {
+            'action': state.running ? 'stop' : 'start',
+            'time': parseInt(dom_min.value) * 60 + parseInt(dom_sec.value)
+        };
+        socket.send(JSON.stringify(action));
+        dom_toggle.blur();
+    });
+}
+
+connect()
